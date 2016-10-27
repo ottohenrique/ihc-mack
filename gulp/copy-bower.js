d60cd02c55cbcd05a6c13a5a54f63ca09b98b409
Clauber Stipkovic <clauber.halic@gmail.com>
2016-10-27 00:42:45 -0200

    Criando o task runner para copiar os arquivos de interface para a pasta correta

---
+++ gulp/copy-bower.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var mainBowerFiles = require('main-bower-files');
+
+gulp.task('copy-bower', function() {
+    return
+        gulp.src(mainBowerFiles(), {
+            base: '../bower_components'
+        })
+        .pipe(gulp.dest('../src/vendor'));
+});
