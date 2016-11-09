e9189909d2ea732f270990f6d9760792c6946aa2
Clauber Stipkovic <cin_chalic@uolinc.com>
2016-11-09 18:37:30 -0200

    inclusao de msg de erros na tela de cadastro

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -16,29 +16,42 @@
     	window.location.assign(link);
     }
 
-    function validar() {
+    function signUpValidation() {
         var email = $('#email').val();
     	var cemail = $('#cemail').val();
     	var senha = $('#senha').val();
-    	var csenha = $('#cenha').val();
+    	var csenha = $('#csenha').val();
+        var userTypeLength = $('input[name=userType]:checked').length;
 
-    	if (email != cemail) {
-    		alert("O e-mail é diferente!");
-    	}
+        console.log(typeof userTypeLength);
 
-    	if (senha != csenha) {
-    		alert("A senha é diferente!");
-    	}
+        if ((email === '') || (senha === '') || (userTypeLength === 0)) {
+            $('#warningEmpty').removeClass('hide');
+        } else {
+            if (email !== cemail) {
+                $('#warningEmail').removeClass('hide');
+        	}
 
-    	if ($("#aluno").attr('checked')) {
-    		redirecionar('./Procurar.html');
-    	} else {
-    		redirecionar('./CadastroProfessor.html');
-    	}
+        	if (senha !== csenha) {
+                $('#warningPass').removeClass('hide');
+        	}
+
+            if (userTypeLength === 0) {
+                $('#warningPass').removeClass('hide');
+            }
+        }
+
+        // TODO: Terminar de implementar a validacao e o redirect para o cadastro
+    	// if ($("#aluno").attr('checked')) {
+    	// 	redirecionar('./Procurar.html');
+    	// } else {
+    	// 	redirecionar('./CadastroProfessor.html');
+    	// }
     }
 
     function main() {
         $('#doLogin').click(doLogin);
+        $('#signUpValidation').click(signUpValidation);
     }
 
     main();
