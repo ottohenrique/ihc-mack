f39fa5c1b37204d2577fc6bf0dd4084067ee9de3
Clauber Stipkovic <cin_chalic@uolinc.com>
2016-11-09 17:22:43 -0200

    Colocando o jquery e colocando a funcao geral como anonima

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,46 +1,53 @@
-function login () {
-	var name = document.getElementById('name').value;
-	var pass = document.getElementById('pass').value;
+(function($) {
+    function doLogin(e) {
+        // console.log(localStorage.getItem('email'));
+        var name = document.getElementById('name').value;
+    	var pass = document.getElementById('pass').value;
 
-	if ((name == 'aluno@teste.com.br' || name == 'professor@teste.com.br') && pass == '123456') {
-		redirecionar('./Procurar.html');
-	} else {
-		if (name == "") {
-			alert('E-mail não preenchido');
-		}else{
-			if (pass == "") {
-				alert('Senha não preenchida');
-			} else { 
-				alert('Usuário ou senha incorreto');
-			}
-		}
-	}
-	
-}
+    	if ((name == 'aluno@teste.com.br' || name == 'professor@teste.com.br') && pass == '123456') {
+    		redirecionar('./Procurar.html');
+    	} else {
+    		if (name == "") {
+    			alert('E-mail não preenchido');
+    		}else{
+    			if (pass == "") {
+    				alert('Senha não preenchida');
+    			} else {
+    				alert('Usuário ou senha incorreto');
+    			}
+    		}
+    	}
+    }
 
-function validar() {
-	var email =  document.getElementById('email').value;
-	var cemail = document.getElementById('cemail').value;
-	var senha = document.getElementById('senha').value;
-	var csenha = document.getElementById('cenha').value;
+    function redirecionar(link) {
+    	window.location.assign(link);
+    }
+
+    function validar() {
+        var email = $('#email').val();
+    	var cemail = $('#cemail').val();
+    	var senha = $('#senha').val();
+    	var csenha = $('#cenha').val();
 
 
-	if (email != cemail) {
-		alert("O e-mail é diferente!");
-	}
+    	if (email != cemail) {
+    		alert("O e-mail é diferente!");
+    	}
 
-	if (senha != csenha) {
-		alert("A senha é diferente!");
-	}
+    	if (senha != csenha) {
+    		alert("A senha é diferente!");
+    	}
 
-	if (document.getElementById("aluno").checked) {
-		redirecionar('./Procurar.html');
-	} else {
-		redirecionar('./CadastroProfessor.html');
-	}
+    	if ($("#aluno").attr('checked')) {
+    		redirecionar('./Procurar.html');
+    	} else {
+    		redirecionar('./CadastroProfessor.html');
+    	}
+    }
 
-}
+    function main() {
+        $('#doLogin').click(doLogin);
+    }
 
-function redirecionar(link) {
-	window.location.assign(link);
-}
+    main();
+})(jQuery);
